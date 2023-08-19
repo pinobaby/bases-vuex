ed9fbb3251bf0a99bb95f89fe31b1e2ef124420f
pinobaby <angel.ismael684@gmail.com>
2023-08-18 20:00:24 -0700

    Curso vue hasta la seccion de getters

---
+++ src/components/Counter.vue
@@ -0,0 +1,58 @@
+<template>
+    <h1>Counter - Vuex</h1>
+    <h2>Direct Access:{{ $store.state.count }}</h2>
+    <h2>Computed:{{ countComputed }}</h2>
+ 
+    <button @click="increment">+1</button>
+    <button @click="incrementBy">+5</button>
+    <button @click="randomInt" v-bind:disabled="isLoading">Random</button>
+    
+
+    <h1>mapState</h1>
+    <h2>mapState: {{ count }}</h2>
+    <h2>lastMutation: {{ lastMutation }}</h2>
+
+    <h2>Direct getter: {{ $store.getters.squareCount }}</h2>
+
+ </template>
+ <script>
+ 
+ import { mapState, mapActions,  } from 'vuex'
+ 
+ export default {  
+     
+    computed: {
+       countComputed(){
+          return this.$store.state.count
+       },
+       ...mapState(['count','lastMutation','isLoading'])
+       //    ...mapState({
+          //    count: state => state.count,
+          //    lastMutation: state => state.lastMutation
+          // })
+         },   
+         methods:{
+           increment(){
+              this.$store.commit('increment')
+            },
+            incrementBy(){
+               this.$store.commit('incrementBy',5)
+
+            },
+            // incrementRandomInt() {
+            //    this.$store.dispatch('incrementRandomInt')
+            // },
+
+            ...mapActions( {
+               randomInt: 'incrementRandomInt',
+               
+
+
+
+            } )
+   
+         }
+      }
+      
+   
+      </script>
